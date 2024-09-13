cee9fef5fa0f332b37ff9ec493c631f118e82629
mohamed <mezoattalla123@gmail.com>
2024-09-13 23:38:48 +0300

    Add color constants for UI styling in Flutter application

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+const kFontColor = Color(0xFF000000);
+const kDarkGreyColor = Color(0xFF50565C);
+const kBlackColor = Color(0xFF1C1B1B);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kLightGreyColor = Color(0xFFE7EAED);
+const kGreyColor = Color(0xFFA0A5AB);
+const kVeryLightGreyColor = Color(0xFFF7F7F7);
