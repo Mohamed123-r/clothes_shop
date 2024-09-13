ac773790d771c354e6455b302e5ace49bf257c70
mohamed <mezoattalla123@gmail.com>
2024-09-13 23:20:15 +0300

    Add Styles class with text styles for

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const heading1Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0,
+  );
+
+  static const heading2Bold = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0,
+  );
+
+  static const heading3Bold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const subTitle1Bold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const subTitle2Bold = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const bodyText1Regular = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const bodyText2Regular = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const caption1Regular = TextStyle(
+    fontSize: 11.0,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const caption2Regular = TextStyle(
+    fontSize: 10.0,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const buttonBold = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.25,
+  );
+
+  static const textButton = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  );
+}
