74dff4ad75b0a55782aaa1e7eeda8d81113f7be6
mohamed <mezoattalla123@gmail.com>
2024-09-16 15:47:57 +0300

    Add height property to TextStyles and implement CustomButton widget

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -5,55 +5,68 @@ abstract class Styles {
     fontSize: 24,
     fontWeight: FontWeight.bold,
     letterSpacing: 0,
+    height: 3.6,
   );
 
   static const heading2Bold = TextStyle(
     fontSize: 21,
     fontWeight: FontWeight.bold,
     letterSpacing: 0,
+    height: 3.2,
   );
 
   static const heading3Bold = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.bold,
+    height: 2.8,
   );
 
   static const subTitle1Bold = TextStyle(
     fontSize: 16.0,
     fontWeight: FontWeight.bold,
+    height: 2.4,
   );
+
   static const subTitle2Bold = TextStyle(
     fontSize: 14.0,
     fontWeight: FontWeight.bold,
+    height: 2.0,
   );
+
   static const bodyText1Regular = TextStyle(
     fontSize: 14.0,
     fontWeight: FontWeight.normal,
+    height: 2,
   );
 
   static const bodyText2Regular = TextStyle(
     fontSize: 12.0,
     fontWeight: FontWeight.normal,
+    height: 1.8,
   );
 
   static const caption1Regular = TextStyle(
     fontSize: 11.0,
     fontWeight: FontWeight.normal,
+    height: 1.6,
   );
 
   static const caption2Regular = TextStyle(
     fontSize: 10.0,
     fontWeight: FontWeight.normal,
+    height: 1.6,
   );
 
   static const buttonBold = TextStyle(
     fontSize: 14.0,
     fontWeight: FontWeight.bold,
     letterSpacing: 0.25,
+    height: 2,
   );
 
   static const textButton = TextStyle(
     fontSize: 12.0,
     fontWeight: FontWeight.bold,
+    height: 1.8,
   );
 }
