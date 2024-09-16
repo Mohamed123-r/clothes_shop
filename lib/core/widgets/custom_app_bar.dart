94699982bb43579a1f58768280f7616fa09f3b4f
mohamed <mezoattalla123@gmail.com>
2024-09-16 16:23:43 +0300

    Add custom AppBar and buttons to the main app layout

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+
+AppBar customAppbar(BuildContext context , { required String title , List<Widget>? actions  }) {
+  return AppBar(
+
+
+    backgroundColor: Colors.transparent,
+    leading: IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(
+        Icons.arrow_back_ios_new_outlined,
+        size: 18,
+      ),
+    ),
+    title:  Text(
+      title ,
+      style: Styles.subTitle1Bold,
+    ),
+    actions: actions,
+  );
+}
