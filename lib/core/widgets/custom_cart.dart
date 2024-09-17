713c6261b312332cd2385d4499a577a70a9a70d5
mohamed <mezoattalla123@gmail.com>
2024-09-17 10:35:51 +0300

    Add CustomCart widget and update main application layout
    - Introduce the CustomCart widget for displaying product information.
    - Update the main application to include a CustomCart in the layout with proper styling and structure.
    - Import necessary widgets and constants for the new functionality.

---
+++ lib/core/widgets/custom_cart.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import '../../constants.dart';
+import '../utils/styles.dart';
+
+class CustomCart extends StatelessWidget {
+  const CustomCart(
+      {super.key,
+      required this.title,
+      required this.subTitle,
+      required this.price,
+      required this.image});
+
+  final String title;
+
+  final String subTitle;
+
+  final String price;
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      width: 160,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: kWhiteColor,
+        border: Border.all(color: kLightGreyColor),
+        boxShadow: const [
+          BoxShadow(
+            color: kLightGreyColor,
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(image, width: 160, height: 150, fit: BoxFit.cover),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  title,
+                  style: Styles.caption2Regular.copyWith(
+                    color: kGreyColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  subTitle,
+                  style: Styles.caption2Regular.copyWith(
+                    color: kGreyColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  price,
+                  style: Styles.subTitle2Bold.copyWith(
+                    color: kDarkGreyColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
