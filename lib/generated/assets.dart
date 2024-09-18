dbede77992e00d3ac400cc961fcf0bd0bd4db2ab
mohamed <mezoattalla123@gmail.com>
2024-09-18 15:52:14 +0300

    Add SplashView and CacheHelper for application initialization
    - Implement SplashView with associated SplashViewBody for initial screen.
    - Introduce CacheHelper class for local data management using SharedPreferences.
    - Add images and constants used in the splash screen.

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -16,6 +16,7 @@ class Assets {
   static const String imagesPasswordIcon = 'assets/images/password_icon.svg';
   static const String imagesPersonIcon = 'assets/images/person_icon.svg';
   static const String imagesSearchIcon = 'assets/images/search_icon.svg';
+  static const String imagesSplashImage = 'assets/images/splashImage.png';
   static const String imagesTest = 'assets/images/test.png';
 
 }
