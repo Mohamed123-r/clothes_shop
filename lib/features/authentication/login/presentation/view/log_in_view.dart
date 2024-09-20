3c97252db5a5fb1b56f2f9df9f8abcdef310a26c
mohamed <mezoattalla123@gmail.com>
2024-09-20 15:21:06 +0300

    Add Login functionality and view
    Implement the LogInView and LogInViewBody for user authentication. Update the AppRouter to include the login route and modify StartViewBody to navigate to the login screen.

---
+++ lib/features/authentication/login/presentation/view/log_in_view.dart
@@ -0,0 +1,21 @@
+import 'package:clothes_shop_app/features/authentication/login/presentation/view/widgets/log_in_view_body.dart';
+import 'package:flutter/material.dart';
+
+class LogInView extends StatelessWidget {
+  const LogInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: const LogInViewBody(),
+    );
+  }
+}
---
+++ lib/features/authentication/login/presentation/view/widgets/log_in_view_body.dart
@@ -0,0 +1,85 @@
+import 'package:clothes_shop_app/constants.dart';
+import 'package:clothes_shop_app/core/utils/styles.dart';
+import 'package:clothes_shop_app/core/widgets/custom_button.dart';
+import 'package:clothes_shop_app/core/widgets/custom_input.dart';
+import 'package:clothes_shop_app/core/widgets/custom_password_input.dart';
+import 'package:clothes_shop_app/generated/assets.dart';
+import 'package:flutter/material.dart';
+
+
+class LogInViewBody extends StatelessWidget {
+  const LogInViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text(
+            'Login',
+            style: Styles.heading2Bold,
+          ),
+          Text(
+            'Enter your information',
+            style: Styles.heading3Bold.copyWith(
+              color: kDarkGreyColor,
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          const CustomInput(
+            hintText: 'Enter your email',
+            labelText: 'Email',
+            prefixIcon: Assets.imagesEmailIcon,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          const CustomPasswordInput(
+            hintText: 'Enter your password',
+            labelText: 'Password',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text('Forgot password?',
+                    style: Styles.bodyText1Regular.copyWith(
+                      color: kGreyColor,
+                    )),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          CustomButton(
+            onPressed: () {},
+            title: 'Login',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Not a member?',
+                style: Styles.bodyText1Regular.copyWith(
+                  color: kGreyColor,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'SIGN UP',
+                  style: Styles.caption1Regular,
+                ),
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
