93711954e7e19a89bbae52de584561511faaa999
mohamed <mezoattalla123@gmail.com>
2024-09-22 19:38:08 +0300

    Add CustomCarouselSlider widget and integrate navigation in LogInViewBody
    - Implement CustomCarouselSlider for displaying a carousel of images.
    - Enhance LogInViewBody with navigation functionality upon button press.

---
+++ lib/features/home/presentation/view/widgets/custom_caroisel_slider.dart
@@ -0,0 +1,28 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+
+class CustomCarouselSlider extends StatelessWidget {
+  const CustomCarouselSlider({
+    super.key, required this.height, required this.imageSliders,  required this.autoPlay,
+  });
+
+  final List<Widget> imageSliders ;
+  final double height ;
+  final  bool autoPlay ;
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: imageSliders,
+      options: CarouselOptions(
+        height: height,
+        autoPlay: autoPlay,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.3,
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
