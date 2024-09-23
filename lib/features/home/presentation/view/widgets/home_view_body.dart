02db66fac92390201f8086fa0ecdb17e1f5c356d
mohamed <mezoattalla123@gmail.com>
2024-09-23 17:43:10 +0300

    Add HomeView and HomeViewBody components for the main screen layout
    - Implement `HomeView` as a stateless widget containing `HomeViewBody`
    - Create `HomeViewBody` with a nested scroll view, custom search bar, and sections for offers, categories, and new arrivals
    - Import necessary packages and components in the appropriate files

---
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,62 @@
+import 'package:clothes_shop_app/constants.dart';
+import 'package:clothes_shop_app/core/widgets/custom_search.dart';
+import 'package:clothes_shop_app/features/home/presentation/view/widgets/sliver_appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'categories_section.dart';
+import 'new_arrival_section.dart';
+import 'offers_section.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: kFontColor,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              const CustomSliverAppBar(),
+            ];
+          },
+          body: Scaffold(
+            body: Column(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.only(bottom: 16, right: 16, left: 16),
+                  decoration: const BoxDecoration(
+                      color: kFontColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(24),
+                        bottomRight: Radius.circular(24),
+                      )),
+                  child: const CustomSearch(),
+                ),
+                const Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        OffersSection(),
+                        CategoriesSection(),
+                        NewArrivalSection(),
+                        SizedBox(
+                          height: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
