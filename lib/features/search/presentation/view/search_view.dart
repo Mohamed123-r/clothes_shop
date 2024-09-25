e6a2f89e94a23805663ad4506f7e4613bb04fd6b
mohamed <mezoattalla123@gmail.com>
2024-09-25 08:36:45 +0300

    Add search view and body with layout and dynamic content
    - Implement `SearchView` and `SearchViewBody` widgets
    - Set up app bar with title and background color
    - Create scrollable layout with search functionality and results display
    - Use `GridView` for product representation with placeholder data

--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -1,10 +1,18 @@
+import 'package:clothes_shop_app/constants.dart';
+import 'package:clothes_shop_app/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'widgets/search_view_body.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kVeryLightGreyColor,
+      appBar: customAppbar(context, title: 'search'),
+      body: const SearchViewBody(),
+    );
   }
 }
+
