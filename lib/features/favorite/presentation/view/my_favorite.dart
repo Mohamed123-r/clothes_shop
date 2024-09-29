a5df5305e52e5c6d141796ded986c198637c480c
mohamed <mezoattalla123@gmail.com>
2024-09-30 02:20:46 +0300

    Add custom cart display in MyFavorite screen
    Import CustomCart widget and Assets, and implement a GridView to display cart items in the MyFavorite screen. Adjust layout with padding and spacing for better UI.

--- lib/features/favorite/presentation/view/my_favorite.dart
+++ lib/features/favorite/presentation/view/my_favorite.dart
@@ -1,4 +1,6 @@
 import 'package:clothes_shop_app/core/widgets/custom_app_bar.dart';
+import 'package:clothes_shop_app/core/widgets/custom_cart.dart';
+import 'package:clothes_shop_app/generated/assets.dart';
 import 'package:flutter/material.dart';
 import 'widgets/taps_section.dart';
 
@@ -9,11 +11,34 @@ class MyFavorite extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppbar(context, title: 'My Favorite'),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
+      body:  Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
-            TapsSection(),
+            const TapsSection(),
+            const SizedBox(height: 24.0),
+            SingleChildScrollView(
+              child: GridView.count(
+                padding: EdgeInsets.zero,
+                crossAxisCount: 2,
+                childAspectRatio: 0.65,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: List.generate(
+                  3,
+                      (index) {
+                    return const CustomCart(
+                      title: 'Muslim Tops Loose',
+                      subTitle: 'Isdal',
+                      price: '1500EG',
+                      image: Assets.imagesTest,
+                    );
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
