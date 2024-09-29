6c8d048f9b562af81e1c6fb49576e8d495c0c907
mohamed <mezoattalla123@gmail.com>
2024-09-29 23:24:23 +0300

    Add account profile and list view sections to AccountView
    - Implement a scrollable layout in AccountView that includes AccountSection and ListViewSection.
    - Create ListViewSection to display multiple account-related options with dividers.
    - Enhance AccountSection by adding profile image, name, email, and an edit button.

--- lib/features/profile/presentation/view/widgets/account_section.dart
+++ lib/features/profile/presentation/view/widgets/account_section.dart
@@ -10,39 +10,42 @@ class AccountSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset(
-          Assets.imagesCategoriesTest,
-          fit: BoxFit.fill,
-          width: 100,
-          height: 100,
-        ),
-        const SizedBox(
-          width: 12,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Mohamed',
-              style: Styles.subTitle1Bold,
-            ),
-            Text(
-              'Mexoattlla123@gmail.com',
-              style: Styles.caption1Regular
-                  .copyWith(color: kDarkGreyColor),
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  'EDIT PROFILE',
-                  style: Styles.caption2Regular
-                      .copyWith(color: kDarkGreyColor),
-                ))
-          ],
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: [
+          Image.asset(
+            Assets.imagesCategoriesTest,
+            fit: BoxFit.fill,
+            width: 70,
+            height: 70,
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Mohamed',
+                style: Styles.subTitle1Bold,
+              ),
+              Text(
+                'Mexoattlla123@gmail.com',
+                style: Styles.caption1Regular
+                    .copyWith(color: kDarkGreyColor),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'EDIT PROFILE',
+                    style: Styles.caption2Regular
+                        .copyWith(color: kDarkGreyColor),
+                  ))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
