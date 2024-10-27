c35d1a7fc720c2cda6c93e3c5021e30bd0fea8bf
mohamed <mezoattalla123@gmail.com>
2024-10-27 17:57:26 +0300

    Implement DioConsumer class for API requests using Dio package

---
+++ lib/core/api/dio_consumer.dart
@@ -0,0 +1,108 @@
+import 'package:clothes_shop_app/core/error/exceptions.dart';
+import 'package:dio/dio.dart';
+import 'api_consumer.dart';
+import 'api_interceptor.dart';
+import 'end_point.dart';
+
+class DioConsumer extends ApiConsumer {
+  final Dio dio;
+
+  DioConsumer({required this.dio}) {
+    dio.options.baseUrl = EndPoint.baseUrl;
+    dio.interceptors.add(ApiInterceptor());
+    dio.interceptors.add(
+      LogInterceptor(
+        error: true,
+        requestBody: true,
+        responseBody: true,
+        requestHeader: true,
+        responseHeader: true,
+      ),
+    );
+  }
+
+  @override
+  Future<dynamic> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    try {
+      var response = await dio.get(
+        path,
+        queryParameters: queryParameters,
+      );
+
+      return response.data;
+    } on DioException catch (e) {
+      ServerFailure(e.toString());
+    }
+  }
+
+  @override
+  Future<dynamic> delete(
+    String path, {
+    Map<String, dynamic>? body,
+    bool isFormData = false,
+  }) async {
+    try {
+      final response = await dio.delete(
+        path,
+        data: isFormData ? FormData.fromMap(body!) : body,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      ServerFailure(e.toString());
+    }
+  }
+
+  @override
+  Future<dynamic> patch(
+    String path, {
+    Map<String, dynamic>? body,
+    bool isFormData = false,
+  }) async {
+    try {
+      final response = await dio.patch(
+        path,
+        data: isFormData ? FormData.fromMap(body!) : body,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      ServerFailure(e.toString());
+    }
+  }
+
+  @override
+  Future<dynamic> post(
+    String path, {
+    Map<String, dynamic>? body,
+    bool isFormData = false,
+  }) async {
+    try {
+      final response = await dio.post(
+        path,
+        data: isFormData ? FormData.fromMap(body!) : body,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      ServerFailure(e.toString());
+    }
+  }
+
+  @override
+  Future<dynamic> put(
+    String path, {
+    Map<String, dynamic>? body,
+    bool isFormData = false,
+  }) async {
+    try {
+      final response = await dio.put(
+        path,
+        data: isFormData ? FormData.fromMap(body!) : body,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      ServerFailure(e.toString());
+    }
+  }
+}
