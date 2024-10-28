3f789c120b1865f15a8a8f3eab5931784031b312
mohamed <mezoattalla123@gmail.com>
2024-10-28 19:42:58 +0300

    Add ProductEntity class to model product data structure

---
+++ lib/features/home/domain/entities/product_entity.dart
@@ -0,0 +1,17 @@
+class ProductEntity {
+  final String id;
+  final String name;
+  final String price;
+  final String image;
+  final String description;
+  final String category;
+
+  const ProductEntity({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.description,
+    required this.category,
+  });
+}
