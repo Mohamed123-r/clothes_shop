c3d64a4868c79dbe4cce52a9a5a71354143de078
mohamed <mezoattalla123@gmail.com>
2024-10-29 15:07:13 +0300

    Implement FetchGetAllProductsUseCase to retrieve all products from the home repository

---
+++ lib/features/home/domain/use_case/fetch_get_all_products_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:clothes_shop_app/core/error/exceptions.dart';
+import 'package:clothes_shop_app/core/use_case/use_case.dart';
+import 'package:clothes_shop_app/features/home/domain/repo/home_repo.dart';
+import 'package:dartz/dartz.dart';
+import '../entities/product_entity.dart';
+
+class FetchGetAllProductsUseCase extends UseCase<List<ProductEntity>, void> {
+  final HomeRepo homeRepo;
+
+  FetchGetAllProductsUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> call([void param]) {
+    return homeRepo.fetchGetAllProducts();
+  }
+}
