26504b44fb4791791f22e0ad5c5fd11e9879ad2e
mohamed <mezoattalla123@gmail.com>
2024-12-30 15:06:22 +0200

    ✨ [feat](deps): Adds get_it, awesome_dialog, logger, and rive
    ✨ [feat](plugins): Adds rive_common plugin

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/animation.dart';
+import 'package:logger/logger.dart';
 
 const kFontColor = Color(0xFF000000);
 const kDarkGreyColor = Color(0xFF50565C);
@@ -7,3 +8,4 @@ const kWhiteColor = Color(0xFFFFFFFF);
 const kLightGreyColor = Color(0xFFE7EAED);
 const kGreyColor = Color(0xFFA0A5AB);
 const kVeryLightGreyColor = Color(0xFFF7F7F7);
+var logger = Logger();
