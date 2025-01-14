a676a2579b7d7e3090f8a7f9e5d4508d5fcbb93f
mohamed <mezoattalla123@gmail.com>
2025-01-14 22:31:32 +0200

    ✨ Feat(cart/my_cart_view): Implement cart items list with slidable delete action ✨ Feat(pubspec.yaml): Add flutter_slidable dependency

--- lib/features/cart/presentation/view/my_cart_view.dart
+++ lib/features/cart/presentation/view/my_cart_view.dart
@@ -1,5 +1,10 @@
 import 'package:clothes_shop_app/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import '../../../../constants.dart';
+import '../../../../core/utils/styles.dart';
+import '../../../../core/widgets/custom_counter.dart';
+import '../../../../generated/assets.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -17,94 +22,115 @@ class MyCartView extends StatelessWidget {
       ]),
       body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child:
-        Column(
+        child: CartListViewItem(),
+      ),
+    );
+  }
+}
+
+class CartListViewItem extends StatelessWidget {
+  const CartListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (context, index) => const CartItem(),
+      separatorBuilder: (context, index) => Container(
+        color: Colors.transparent,
+        height: 8,
+      ),
+      itemCount: 10,
+    );
+  }
+}
+
+class CartItem extends StatelessWidget {
+  const CartItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: kLightGreyColor,
+            foregroundColor: Colors.yellow,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+            spacing: 40,
+            flex: 2,
+            icon: Icons.delete,
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: kWhiteColor,
+          border: Border.all(color: kLightGreyColor),
+        ),
+        child: Row(
           children: [
-            // Slidable(
-            //
-            //   endActionPane: ActionPane(
-            //     motion: const ScrollMotion(),
-            //     children: [
-            //       SlidableAction(
-            //
-            //         onPressed: (context) {},
-            //         backgroundColor:kLightGreyColor ,
-            //         foregroundColor:Colors.red ,
-            //         borderRadius:  const BorderRadius.only(
-            //           topRight: Radius.circular(16),
-            //           bottomRight: Radius.circular(16),
-            //         ) ,
-            //         spacing: 40,
-            //         flex: 2,
-            //         icon: Icons.delete,
-            //       ),
-            //     ],
-            //   ),
-            //   child:  Container(
-            //     decoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(16),
-            //       color: kWhiteColor,
-            //       border: Border.all(color: kLightGreyColor),
-            //       boxShadow: const [
-            //         BoxShadow(
-            //           color: kLightGreyColor,
-            //           spreadRadius: 5,
-            //           blurRadius: 7,
-            //           offset: Offset(0, 3), // changes position of shadow
-            //         ),
-            //       ],
-            //     ),
-            //     child: Row(
-            //       children: [
-            //         Padding(
-            //           padding: const EdgeInsets.all(8.0),
-            //           child: Image.asset(Assets.imagesProducteDetailsTest,
-            //               width: 85, height: 85, fit: BoxFit.cover),
-            //         ),
-            //         Padding(
-            //           padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            //           child: Column(
-            //             crossAxisAlignment: CrossAxisAlignment.start,
-            //             children: [
-            //               const SizedBox(
-            //                 height: 8,
-            //               ),
-            //               const Text('T-Shirt', style: Styles.subTitle1Bold),
-            //               const SizedBox(
-            //                 height: 8,
-            //               ),
-            //               Text(
-            //                 ' T-Shirt',
-            //                 style: Styles.caption2Regular.copyWith(
-            //                   color: kGreyColor,
-            //                 ),
-            //               ),
-            //               const SizedBox(
-            //                 height: 8,
-            //               ),
-            //               Row(
-            //                 mainAxisSize: MainAxisSize.max,
-            //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //                 children: [
-            //                   Text(
-            //                     '\$ 10.00',
-            //                     style: Styles.subTitle2Bold.copyWith(
-            //                       color: kDarkGreyColor,
-            //                     ),
-            //                   ),
-            //                   const SizedBox(
-            //                     width: 48,
-            //                   ),
-            //                   const CustomCounter(),
-            //                 ],
-            //               ),
-            //             ],
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  Assets.imagesProducteDetailsTest,
+                  width: 85,
+                  height: 85,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text('T-Shirt', style: Styles.subTitle1Bold),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    ' T-Shirt',
+                    style: Styles.caption2Regular.copyWith(
+                      color: kGreyColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$ 10.00',
+                        style: Styles.subTitle2Bold.copyWith(
+                          color: kDarkGreyColor,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 48,
+                      ),
+                      const CustomCounter(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
