4ebb66560117d9f9bd8453d612c75378db0bd6f2
mohamed <mezoattalla123@gmail.com>
2025-01-22 22:54:51 +0200

    ✨ [FEAT](cart/profile): Add CartEntity and ProfileModel classes

---
+++ lib/features/profile/data/models/profile_model.dart
@@ -0,0 +1,43 @@
+import 'package:clothes_shop_app/features/profile/domain/entities/profile_entity.dart';
+
+class ProfileModel {
+  ProfileModel({
+    this.name,
+    this.email,
+    this.phone,
+    this.location,
+    this.gender,
+  });
+
+  ProfileModel.fromJson(dynamic json) {
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    location = json['location'];
+    gender = json['gender'];
+  }
+
+  String? name;
+  String? email;
+  String? phone;
+  String? location;
+  num? gender;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['name'] = name;
+    map['email'] = email;
+    map['phone'] = phone;
+    map['location'] = location;
+    map['gender'] = gender;
+    return map;
+  }
+
+  ProfileEntity toEntity() => ProfileEntity(
+        name: name!,
+        email: email!,
+        phone: phone!,
+        location: location!,
+        gender: gender!,
+      );
+}
